877c7e09bd2f9bc52fd6dd76bdcf7b11ca6b1f7f
bhumitsoni20 <sbhumit77@gmail.com>
2026-01-14 22:34:47 +0530

    fixed some error in navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -58,17 +58,17 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      
+
       <div className="navbar-main">
         <div className="container">
           <button className="mobile-menu-btn" onClick={() => setIsOpen(!isOpen)}>
             <FaBars />
           </button>
-          
+
           <ul className="nav-menu flex items-center">
             {navLinks.map((link, index) => (
-              <li 
-                key={index} 
+              <li
+                key={index}
                 className="nav-item"
                 onMouseEnter={() => handleMouseEnter(link.title)}
                 onMouseLeave={handleMouseLeave}
@@ -81,88 +81,86 @@ const Navbar = () => {
                 {/* Specific Layout for "Browse by Stream" */}
                 <AnimatePresence>
                   {link.title === 'Browse by Stream' && activeDropdown === 'Browse by Stream' && (
-                    <motion.div 
+                    <motion.div
                       initial={{ opacity: 0, y: 10 }}
                       animate={{ opacity: 1, y: 0 }}
                       exit={{ opacity: 0, y: 10 }}
                       transition={{ duration: 0.2 }}
                       className="mega-menu split-layout"
                     >
-                       <div className="mega-menu-sidebar">
-                          {browseByStreamData.map((stream) => (
-                            <div 
-                              key={stream.id}
-                              className={`sidebar-item ${activeStream === stream.id ? 'active' : ''}`}
-                              onMouseEnter={() => setActiveStream(stream.id)}
-                            >
-                              {stream.label}
-                              <FaAngleRight className="sidebar-arrow" />
-                            </div>
-                          ))}
-                       </div>
-                       
-                       <div className="mega-menu-content">
-                          <div className="content-grid-3">
-                             {/* Column 1: Exams */}
-                             <div className="content-col">
-                                <h4 className="col-heading">{currentStreamDataObj.titles?.col1 || 'Exams'}</h4>
-                                <ul className="content-list">
-                                  {currentStreamContent.exams.map((item, idx) => (
-                                    <li key={idx}>
-                                      <a href={item.href} className={`content-link ${item.isLink ? 'highlight' : ''}`}>
-                                        {item.title}
-                                      </a>
-                                    </li>
-                                  ))}
-                                </ul>
-                             </div>
-
-                             {/* Column 2: Colleges */}
-                             <div className="content-col">
-                                <h4 className="col-heading">{currentStreamDataObj.titles?.col2 || 'Colleges'}</h4>
-                                <ul className="content-list">
-                                  {currentStreamContent.colleges.map((item, idx) => (
-                                    <li key={idx}>
-                                      <a href={item.href} className="content-link">
-                                        {item.title}
-                                      </a>
-                                    </li>
-                                  ))}
-                                </ul>
-                             </div>
-
-                             {/* Column 3: Predictors & Resources */}
-                             <div className="content-col">
-                                <h4 className="col-heading">{currentStreamDataObj.titles?.col3_1 || 'Predictors'}</h4>
-                                <ul className="content-list mb-4">
-                                  {currentStreamContent.predictors.map((item, idx) => (
-                                    <li key={idx}>
-                                      <a href={item.href} className={`content-link ${item.isLink ? 'highlight' : ''}`}>
-                                        {item.title}
-                                      </a>
-                                    </li>
-                                  ))}
-                                </ul>
-
-                                <h4 className="col-heading">{currentStreamDataObj.titles?.col3_2 || 'Resources'}</h4>
-                                <ul className="content-list">
-                                  {currentStreamContent.resources.map((item, idx) => (
-                                    <li key={idx}>
-                                      <a href={item.href} className="content-link">
-                                        {item.title}
-                                      </a>
-                                    </li>
-                                  ))}
-                                </ul>
-                                
-                                <div className="app-download-banner">
-                                  <FaDownload /> Download App
-                                </div>
-                             </div>
+                      <div className="mega-menu-sidebar">
+                        {browseByStreamData.map((stream) => (
+                          <div
+                            key={stream.id}
+                            className={`sidebar-item ${activeStream === stream.id ? 'active' : ''}`}
+                            onMouseEnter={() => setActiveStream(stream.id)}
+                          >
+                            {stream.label}
+                            <FaAngleRight className="sidebar-arrow" />
                           </div>
-                       </div>
-                    </motion.div>
-                  )}
+                        ))}
+                      </div>
+
+                      <div className="mega-menu-content">
+                        <div className="content-grid-3">
+                          {/* Column 1: Exams */}
+                          <div className="content-col">
+                            <h4 className="col-heading">{currentStreamDataObj.titles?.col1 || 'Exams'}</h4>
+                            <ul className="content-list">
+                              {currentStreamContent.exams.map((item, idx) => (
+                                <li key={idx}>
+                                  <a href={item.href} className={`content-link ${item.isLink ? 'highlight' : ''}`}>
+                                    {item.title}
+                                  </a>
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+
+                          {/* Column 2: Colleges */}
+                          <div className="content-col">
+                            <h4 className="col-heading">{currentStreamDataObj.titles?.col2 || 'Colleges'}</h4>
+                            <ul className="content-list">
+                              {currentStreamContent.colleges.map((item, idx) => (
+                                <li key={idx}>
+                                  <a href={item.href} className="content-link">
+                                    {item.title}
+                                  </a>
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+
+                          {/* Column 3: Predictors & Resources */}
+                          <div className="content-col">
+                            <h4 className="col-heading">{currentStreamDataObj.titles?.col3_1 || 'Predictors'}</h4>
+                            <ul className="content-list mb-4">
+                              {currentStreamContent.predictors.map((item, idx) => (
+                                <li key={idx}>
+                                  <a href={item.href} className={`content-link ${item.isLink ? 'highlight' : ''}`}>
+                                    {item.title}
+                                  </a>
+                                </li>
+                              ))}
+                            </ul>
+
+                            <h4 className="col-heading">{currentStreamDataObj.titles?.col3_2 || 'Resources'}</h4>
+                            <ul className="content-list">
+                              {currentStreamContent.resources.map((item, idx) => (
+                                <li key={idx}>
+                                  <a href={item.href} className="content-link">
+                                    {item.title}
+                                  </a>
+                                </li>
+                              ))}
+                            </ul>
+
+                            <div className="app-download-banner">
+                              <FaDownload /> Download App
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </motion.div>
                   )}
                 </AnimatePresence>
@@ -170,92 +168,92 @@ const Navbar = () => {
                 {/* Test Prep Mega Menu */}
                 <AnimatePresence>
                   {link.title === 'Test Prep' && activeDropdown === 'Test Prep' && (
-                    <motion.div 
+                    <motion.div
                       initial={{ opacity: 0, y: 10 }}
                       animate={{ opacity: 1, y: 0 }}
                       exit={{ opacity: 0, y: 10 }}
                       transition={{ duration: 0.2 }}
                       className="mega-menu split-layout"
                     >
-                       <div className="mega-menu-sidebar">
-                          {testPrepData.map((stream) => (
-                            <div 
-                              key={stream.id}
-                              className={`sidebar-item ${activeTestPrepStream === stream.id ? 'active' : ''}`}
-                              onMouseEnter={() => setActiveTestPrepStream(stream.id)}
-                            >
-                              {stream.label}
-                              <FaAngleRight className="sidebar-arrow" />
-                            </div>
-                          ))}
-                       </div>
-                       
-                       <div className="mega-menu-content">
-                          <div className="content-grid-3">
-                             <div className="content-col">
-                                <h4 className="col-heading">{currentTestPrepDataObj.titles?.col1 || 'Exam Prep'}</h4>
-                                <ul className="content-list">
-                                  {currentTestPrepContent.exams?.map((item, idx) => (
-                                    <li key={idx}>
-                                      <a href={item.href} className={`content-link ${item.isLink ? 'highlight' : ''}`}>
-                                        {item.title}
-                                      </a>
-                                    </li>
-                                  ))}
-                                </ul>
-                             </div>
-
-                             <div className="content-col">
-                                <h4 className="col-heading">{currentTestPrepDataObj.titles?.col2 || 'Mock Tests'}</h4>
-                                <ul className="content-list">
-                                  {currentTestPrepContent.colleges?.map((item, idx) => (
-                                    <li key={idx}>
-                                      <a href={item.href} className="content-link">
-                                        {item.title}
-                                      </a>
-                                    </li>
-                                  ))}
-                                </ul>
-                             </div>
-
-                             <div className="content-col">
-                                <h4 className="col-heading">{currentTestPrepDataObj.titles?.col3_1 || 'Previous Papers'}</h4>
-                                <ul className="content-list mb-4">
-                                  {currentTestPrepContent.predictors?.map((item, idx) => (
-                                    <li key={idx}>
-                                      <a href={item.href} className={`content-link ${item.isLink ? 'highlight' : ''}`}>
-                                        {item.title}
-                                      </a>
-                                    </li>
-                                  ))}
-                                </ul>
-
-                                <h4 className="col-heading">{currentTestPrepDataObj.titles?.col3_2 || 'Resources'}</h4>
-                                <ul className="content-list">
-                                  {currentTestPrepContent.resources?.map((item, idx) => (
-                                    <li key={idx}>
-                                      <a href={item.href} className="content-link">
-                                        {item.title}
-                                      </a>
-                                    </li>
-                                  ))}
-                                </ul>
-                                
-                                <div className="app-download-banner">
-                                  <FaDownload /> Download App
-                                </div>
-                             </div>
+                      <div className="mega-menu-sidebar">
+                        {testPrepData.map((stream) => (
+                          <div
+                            key={stream.id}
+                            className={`sidebar-item ${activeTestPrepStream === stream.id ? 'active' : ''}`}
+                            onMouseEnter={() => setActiveTestPrepStream(stream.id)}
+                          >
+                            {stream.label}
+                            <FaAngleRight className="sidebar-arrow" />
                           </div>
-                       </div>
+                        ))}
+                      </div>
+
+                      <div className="mega-menu-content">
+                        <div className="content-grid-3">
+                          <div className="content-col">
+                            <h4 className="col-heading">{currentTestPrepDataObj.titles?.col1 || 'Exam Prep'}</h4>
+                            <ul className="content-list">
+                              {currentTestPrepContent.exams?.map((item, idx) => (
+                                <li key={idx}>
+                                  <a href={item.href} className={`content-link ${item.isLink ? 'highlight' : ''}`}>
+                                    {item.title}
+                                  </a>
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+
+                          <div className="content-col">
+                            <h4 className="col-heading">{currentTestPrepDataObj.titles?.col2 || 'Mock Tests'}</h4>
+                            <ul className="content-list">
+                              {currentTestPrepContent.colleges?.map((item, idx) => (
+                                <li key={idx}>
+                                  <a href={item.href} className="content-link">
+                                    {item.title}
+                                  </a>
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+
+                          <div className="content-col">
+                            <h4 className="col-heading">{currentTestPrepDataObj.titles?.col3_1 || 'Previous Papers'}</h4>
+                            <ul className="content-list mb-4">
+                              {currentTestPrepContent.predictors?.map((item, idx) => (
+                                <li key={idx}>
+                                  <a href={item.href} className={`content-link ${item.isLink ? 'highlight' : ''}`}>
+                                    {item.title}
+                                  </a>
+                                </li>
+                              ))}
+                            </ul>
+
+                            <h4 className="col-heading">{currentTestPrepDataObj.titles?.col3_2 || 'Resources'}</h4>
+                            <ul className="content-list">
+                              {currentTestPrepContent.resources?.map((item, idx) => (
+                                <li key={idx}>
+                                  <a href={item.href} className="content-link">
+                                    {item.title}
+                                  </a>
+                                </li>
+                              ))}
+                            </ul>
+
+                            <div className="app-download-banner">
+                              <FaDownload /> Download App
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </motion.div>
                   )}
                 </AnimatePresence>
 
                 {/* Generic Dropdown for others (simpler) */}
                 {link.title !== 'Browse by Stream' && link.hasDropdown && activeDropdown === link.title && (
-                   <div className="simple-dropdown">
-                      {/* ... other dropdowns can be implemented similarly ... */}
-                   </div>
+                  <div className="simple-dropdown">
+                    {/* ... other dropdowns can be implemented similarly ... */}
+                  </div>
                 )}
               </li>
             ))}
